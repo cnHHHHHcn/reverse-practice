846230fe0f952317fb82379259dbdc5f8b2b57d8
cnHHHHHcn <2273194336@qq.com>
2026-06-07 12:38:36 +0800

    Add files via upload

--- demoTest/Stack Project/StackHijack.cpp
+++ demoTest/Stack Project/StackHijack.cpp
@@ -1,6 +1,6 @@
 /**
  * @file StackHijack.cpp
- * @brief x86 栈返回地址劫持与 Shellcode 注入实验
+ * @brief x86 栈返回地址劫持与 Shellcode + ROP 注入实验
  *
  * @details
  * 本文件演示了通过直接修改函数栈帧中的返回地址（Return Address），
@@ -8,7 +8,9 @@
  * 核心机制包括：
  * 1. 【栈地址推算】：利用函数参数地址反推栈上返回地址的物理位置。
  * 2. 【内存读写原语】：使用 memcpy 构造任意地址读写（ARW），备份原返回地址。
- * 3. 【Shellcode 注入】：在栈上布置包含业务逻辑（修改 EAX）的机器码。
+ * 3. 【混合式利用 (Hybrid Exploit)】：
+ *		- 利用 ROP 链（Push + Ret）模拟 call 调用约束并触发 MessageBoxA
+ *		- 结合 Shellcode 注入实现函数返回值(EAX)篡改与流恢复。
  * 4. 【控制流恢复】：在 Shellcode 中动态修补原返回地址并执行 ret，维持程序稳定性。
  *
  * 【技术特征 / Technical Notes】
@@ -37,12 +39,12 @@
  *       1. 【见证劫持时刻】：在 Add 函数末尾，观察栈内存窗口。
  *          确认原本指向 main 的返回地址已被替换为 ShellcodeAddress。
  *       2. 【追踪 Shellcode 执行】：单步执行 ret 指令。
- *          观察 EIP/RIP 寄存器是否跳转至 Shellcode 区域，并验证 EAX 寄存器是否被修改为 32。
+ *          观察 EIP/RIP 寄存器是否跳转至 Shellcode 区域，触发 MessageBoxA 并验证 EAX 寄存器是否被修改为 32。
  *       3. 【验证恢复逻辑】：在 Shellcode 的 ret 指令后，确认程序流是否正确返回 main 继续执行。
  *
  * @author cnHHHHHcn
- * @date 2026-06-02
- * @version 1.0 (Experimental - Stack Control Flow Hijacking)
+ * @date 2026-06-07
+ * @version 1.1 (Experimental - Stack Control Flow Hijacking)
  */
 
 // 编译 Release x86
@@ -59,6 +61,9 @@
 #include <windows.h>
 #include <iostream>
 
+ // 劫持成功后 MessageBoxA 的标题和内容
+const char* HackedTitle = "Stack hijack";
+const char* HackedPropmt = "I hijacked the Add function, tampered with its return address, and successfully triggered MessageBoxA.";
 
 void* HaijackStackAddress = nullptr;	// 需要劫持的栈地址
 void* ReturnAddress = nullptr;			// 原返回地址
@@ -68,13 +73,13 @@ void* ShellcodeAddress = nullptr;		// shellcode地址
 /**
  * Flow Overview:
  *  Original flow: main() -> Add() -> return to main()
- *  Hijacked flow: main() -> Add() -> 恶意shellcode -> return to main()
+ *  Hijacked flow: main() -> Add() -> 恶意shellcode -> User32.dll!MessageBoxA -> 恶意shellcode -> return to main()
  * 
  *  注：_cdecl和_stdcall的区别在于栈清理方式，但对于本示例中的劫持原理没有影响。
  *	   因为我们直接修改了返回地址，无论使用哪种调用约定，都会劫持到shellcode执行。
  */
 
-int /* _cdecl(默认) | _stdcall */ Add(int Num1, int Num2) {
+int _stdcall /* _cdecl(默认) | _stdcall */ Add(int Num1, int Num2) {
 	
 	// 通过获取Num1的地址，获取 返回地址 的栈地址(HaijackStackAddress)
 	HaijackStackAddress = &Num1;
@@ -82,7 +87,7 @@ int /* _cdecl(默认) | _stdcall */ Add(int Num1, int Num2) {
 	
 	// 保存返回地址(ReturnAddress)，以便 恶意shellcode 执行完后能正确返回到main函数继续执行
 	ReturnAddress = *(void**)HaijackStackAddress;		
-	memcpy(((char*)ShellcodeAddress + 6), &ReturnAddress, 4);
+	memcpy(((char*)ShellcodeAddress + 34), &ReturnAddress, 4);
 	
 	// 将shellcode地址写入返回地址，劫持程序流
 	memcpy(HaijackStackAddress, &ShellcodeAddress, 4);
@@ -91,11 +96,20 @@ int /* _cdecl(默认) | _stdcall */ Add(int Num1, int Num2) {
 }
 
 int main() {
-	// 恶意shellcode功能：将eax寄存器设置为32，模拟返回值；将 原本的返回地址 压入栈中，模拟函数参数；ret指令返回到原地址继续执行
-	char shellcode[11] = {
-		0xB8, 0x20, 0x00, 0x00, 0x00, // mov eax, 32
-		0x68, 0x00, 0x00, 0x00, 0x00, // push 0x00000000(占位符 ReturnAddress)
-		0xC3                          // ret
+	// 恶意shellcode功能: 调用MessageBoxA，参数为 (NULL, HackedPropmt, HackedTitle, MB_OK | MB_ICONINFORMATION)，并将EAX寄存器(返回值)修改为32
+	// 注：在shellcode汇编指令中的 5 - 7 行的指令模拟了 call MessageBoxA 的行为，手动压入参数并通过 ret 触发调用，实现控制流重定向。
+	// 如果不理解可以参考 AsmNote.txt x86汇编指令解析
+	unsigned char shellcode[] = {
+		0x68, 0x40, 0x00, 0x00, 0x00,		// push 0x00000040 (MB_OK | MB_ICONINFORMATION)
+		0x68, 0x00, 0x00, 0x00, 0x00,		// push 0x00000000 (HackedTitle Address)
+		0x68, 0x00, 0x00, 0x00, 0x00,		// push 0x00000000 (HackedPrompt Address)
+		0x6A, 0x00,							// push 0 (NULL)
+		0x68, 0x00, 0x00, 0x00, 0x00,		// push 0x00000000 (Return Address = shellcode Address + 28 => [mov eax, 32])
+		0x68, 0x00, 0x00, 0x00, 0x00,		// push 0x00000000 (MessageBoxA Address)
+		0xC3,								// ret
+		0xB8, 0x20, 0x00, 0x00, 0x00,		// mov eax, 32
+		0x68, 0x00, 0x00, 0x00, 0x00,		// push 0x00000000 (ReturnAddress)
+		0xC3								// ret
 	};
 
 	// 修改内存权限，使shellcode可执行
@@ -103,11 +117,30 @@ int main() {
 	ShellcodeAddress = shellcode;
 	VirtualProtect(shellcode, sizeof(shellcode), PAGE_EXECUTE_READWRITE, &oldProtect);
 	
+	// 填充shellcode中的地址
+	memcpy(((char*)shellcode + 6), &HackedTitle, 4);					// shellcode中push的 第二个参数地址
+	memcpy(((char*)shellcode + 11), &HackedPropmt, 4);					// shellcode中push的 第三个参数地址
+	void* ReturnAddressForShellcode = ((char*)ShellcodeAddress + 28);	// shellcode中push的 返回地址，指向shellcode中mov eax, 32的指令
+	memcpy(((char*)shellcode + 18), &ReturnAddressForShellcode, 4);		// shellcode中push的 第四个参数地址
+	HMODULE hUser32 = LoadLibraryA("user32.dll");
+	if(hUser32 == NULL) {
+		std::cerr << "Failed to load user32.dll" << std::endl;
+		return -1;
+	}
+	FARPROC pMsgBox = GetProcAddress(hUser32, "MessageBoxA");
+	if(pMsgBox == NULL) {
+		std::cerr << "Failed to get MessageBoxA address" << std::endl;
+		FreeLibrary(hUser32);
+		return -1;
+	}
+	memcpy(((char*)shellcode + 23), &pMsgBox, 4);					// shellcode中push的 MessageBoxA 地址
+	
 	// 调用Add函数，触发劫持
 	int a = 1, b = 2;
 	int c = Add(a, b);
 
 	// 输出结果，应该输出32，而不是3
 	std::cout << c;
+	FreeLibrary(hUser32);
 	return 0;
 }
